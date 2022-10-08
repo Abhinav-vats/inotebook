70bbda51a8743ad1960c5fccf0a42c6c11a98888
Abhinav-vats <abhi123up@gmail.com>
2022-10-08 21:43:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <>
+    <div>
+      Hello React
+    </div>
+    </>
   );
 }
 
